53335acfd445e5eb21b4510088435721597d27c5
NaokoKawa <39685448+NaokoKawa@users.noreply.github.com>
2020-08-06 17:13:10 +0900

    Add files via upload
    challenge

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from slackeventsapi import SlackEventAdapter
 
 app = Flask(__name__)
 
-slack_events_adapter = SlackEventAdapter("SIGINING_SECRET", "/slack/events", app)
+slack_events_adapter = SlackEventAdapter("SIGINING_SECRET", "7653aef4d589b149c94a47cb8b848dd8", app)
 @slack_events_adapter.on("message")
 def hookSlackEvents(event_data):
     app.logger.debug(event_data)
