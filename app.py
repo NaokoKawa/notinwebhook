236a2954b7c700250990d7f05ee1d11d8f9e6120
NaokoKawa <39685448+NaokoKawa@users.noreply.github.com>
2020-08-06 14:21:22 +0900

    Add files via upload
    challenge認証

--- app.py
+++ app.py
@@ -1,32 +1,7 @@
-import flask
-from flask import request, Response
-import os
-import json
-
-app = flask.Flask(__name__)
-
-
-@app.route('/', methods=["POST"])
-def index():
-    data = request.data.decode('utf-8')
-    data = json.loads(data)
-    # for challenge of slack api
-    if 'challenge' in data:
-        token = str(data['challenge'])
-        return Response(token, mimetype='text/plane')
-    # for events which you added
-    if 'event' in data:
-        print("get event")
-        event = data['event']
-        if 'user' in event:
-            print("user = ", event["user"])
-        if "text" in event:
-            print("text = ", event["text"])
-    return Response("nothing", mimetype='text/plane')
-
-
-port = os.getenv('VCAP_APP_PORT', '8000')
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(port), debug=True)
+def lambda_handler(event, context):
+    
+    # SlackÇÃEvent APIÇÃîFèÿ
+    if "challenge" in event:
+        return event["challenge"]
+    
+    return "OK"    
